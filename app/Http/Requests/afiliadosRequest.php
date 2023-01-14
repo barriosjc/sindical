e20b93312ff0306b9f1e9af692552b98413d6467
barriosjc <juan.barrios@reserv.com.ar>
2023-01-14 20:34:44 -0300

    validando dni, cuil, nro afil unique

--- app/Http/Requests/afiliadosRequest.php
+++ app/Http/Requests/afiliadosRequest.php
@@ -29,8 +29,8 @@ class afiliadosRequest extends FormRequest
         // dd($rq->fecha_ing_empr, $rq->fecha_egr_empr);
         return [
             'sexo' => ['required'],
-            'telefono1' => ['required'],           
-            'calle' => ['required'],            
+            'telefono1' => ['required'],
+            'calle' => ['required'],
             'calle_nro' => ['required'],
             'obra_social_id' => ['required'],
             'afil_estado_ficha_id' => ['required'],
@@ -40,79 +40,116 @@ class afiliadosRequest extends FormRequest
             'apellido_nombres' => ['required', 'max:150'],
             'tipo_documento_id' => ['required'],
             'nro_doc' => ['required', 'numeric', 'digits_between:6,12'],
-            'fecha_egreso_os' => ['nullable','required_with:motivo_egreso_os_id', 'date', 'before_or_equal:today'],
+            'fecha_egreso_os' => ['nullable', 'required_with:motivo_egreso_os_id', 'date', 'before_or_equal:today'],
             'motivo_egreso_os_id' => ['required_with:fecha_egreso_os'],
             'seccional_id' => ['required'],
             'empresa_id' => ['required'],
-            'fecha_ingreso' => ['bail', 'required','required_with:fecha_egreso', 'date', 'before_or_equal:today'],
-            'fecha_ing_empr' => ['bail', 'required','required_with:fecha_egr_empr', 'date', 'before_or_equal:today'],
-            'delegado_desde' => ['bail', 'nullable','required_with:delegado_hasta', 'date', 'before_or_equal:today'],
-            'motivo_egreso_id' => [function ($attribute, $value, $fail) use ($rq) {
-                if (!($rq->input($attribute) == null)) {
-                    if ($rq->input('fecha_egreso') == null) {
-                        $fail('Si seleccionó Motivo de egreso es obligatorio el ingreso de Fecha de baja');
-                    }
-                }
-            }
-            ],
-
-            'fecha_egreso' => ['bail', 
+            'fecha_ingreso' => ['bail', 'required', 'required_with:fecha_egreso', 'date', 'before_or_equal:today'],
+            'fecha_ing_empr' => ['bail', 'required', 'required_with:fecha_egr_empr', 'date', 'before_or_equal:today'],
+            'delegado_desde' => ['bail', 'nullable', 'required_with:delegado_hasta', 'date', 'before_or_equal:today'],
+            'motivo_egreso_id' => [
                 function ($attribute, $value, $fail) use ($rq) {
-                // dd($attribute, $rq->input('fecha_ing_empr'), $value, $value < $rq->input('fecha_ing_empr'));
-                if (!($rq->input($attribute) == null)) {
-                    if ($rq->input('motivo_egreso_id') == null) {
-                        $fail('Si ingresa fecha de egreso es obligatorio seleccionar el Motivo de egreso');
-                    }
-                    if ($value < $rq->input('fecha_ingreso')) {
-                        $fail('la Fecha de afiliación a la empresa debe ser menor a la Fecha de baja');
+                    if (!($rq->input($attribute) == null)) {
+                        if ($rq->input('fecha_egreso') == null) {
+                            $fail('Si seleccionó Motivo de egreso es obligatorio el ingreso de Fecha de baja');
+                        }
                     }
                 }
-            }
             ],
 
-            'fecha_egr_empr' => [function ($attribute, $value, $fail) use ($rq) {
-                // dd($attribute, $rq->input('fecha_ing_empr'), $value, $value < $rq->input('fecha_ing_empr'));
-                if (!($rq->input($attribute) == null)) {
-                    if ($rq->input('fecha_ing_empr') == null) {
-                        $fail('Es obligatorio el ingreso de la fecha de ingreso');
-                    }
-                    if ($value < $rq->input('fecha_ing_empr')) {
-                        $fail('la Fecha de ingreso a la empresa debe ser menor a la Fecha de egreso');
+            'fecha_egreso' => [
+                'bail',
+                function ($attribute, $value, $fail) use ($rq) {
+                    // dd($attribute, $rq->input('fecha_ing_empr'), $value, $value < $rq->input('fecha_ing_empr'));
+                    if (!($rq->input($attribute) == null)) {
+                        if ($rq->input('motivo_egreso_id') == null) {
+                            $fail('Si ingresa fecha de egreso es obligatorio seleccionar el Motivo de egreso');
+                        }
+                        if ($value < $rq->input('fecha_ingreso')) {
+                            $fail('la Fecha de afiliación a la empresa debe ser menor a la Fecha de baja');
+                        }
                     }
                 }
-            }
             ],
 
-            'delegado_hasta' => [function ($attribute, $value, $fail) use ($rq) {
-                // dd($attribute, $rq->input('fecha_ing_empr'), $value, $value < $rq->input('fecha_ing_empr'));
-                if (!($rq->input($attribute) == null)) {
-                    if ($rq->input('delegado_desde') == null) {
-                        $fail('Es obligatorio el ingreso de la fecha de comienzo como delegado');
-                    }
-                    if ($value < $rq->input('delegado_desde')) {
-                        $fail('la Fecha de comienzo como delegado en la empresa debe ser menor a la Fecha de finalización de delegado');
+            'fecha_egr_empr' => [
+                function ($attribute, $value, $fail) use ($rq) {
+                    // dd($attribute, $rq->input('fecha_ing_empr'), $value, $value < $rq->input('fecha_ing_empr'));
+                    if (!($rq->input($attribute) == null)) {
+                        if ($rq->input('fecha_ing_empr') == null) {
+                            $fail('Es obligatorio el ingreso de la fecha de ingreso');
+                        }
+                        if ($value < $rq->input('fecha_ing_empr')) {
+                            $fail('la Fecha de ingreso a la empresa debe ser menor a la Fecha de egreso');
+                        }
                     }
                 }
-            }
             ],
 
-            'nro_doc' => [function ($attribute, $value, $fail) use ($rq) {
-                if (!($rq->input($attribute) == null)) {
-                    if(! $rq->id){
-                        $afiliado = afiliado::where("nro_doc", $rq->nro_doc)->first(); 
-                        if (!empty($afiliado)) {
-                            $fail("Otro afiliado titular ya esta dado de alta con el mismo nro de DNI");
-                        }   
+            'delegado_hasta' => [
+                function ($attribute, $value, $fail) use ($rq) {
+                    // dd($attribute, $rq->input('fecha_ing_empr'), $value, $value < $rq->input('fecha_ing_empr'));
+                    if (!($rq->input($attribute) == null)) {
+                        if ($rq->input('delegado_desde') == null) {
+                            $fail('Es obligatorio el ingreso de la fecha de comienzo como delegado');
+                        }
+                        if ($value < $rq->input('delegado_desde')) {
+                            $fail('la Fecha de comienzo como delegado en la empresa debe ser menor a la Fecha de finalización de delegado');
+                        }
                     }
-                    $gru_fam = grupo_familiar::where("nro_doc", $value)->first();
-                    if ($gru_fam) {
-                        if (empty($gru_fam->fecha_egreso_sind)){
-                            $fail("Error al dar de alta, Ya existe un Familiar con este dni, primero de de baja el familiar y luego de alta el titular.");
-                        }                
-                    } 
                 }
-            }
-        ],
+            ],
+
+            'nro_doc' => [
+                function ($attribute, $value, $fail) use ($rq) {
+                    if (!($rq->input($attribute) == null)) {
+                        if (!$rq->id) {
+                            $afiliado = afiliado::where("nro_doc", $rq->nro_doc)->first();
+                            if (!empty($afiliado)) {
+                                $fail("Otro afiliado titular ya esta dado de alta con el mismo nro de DNI");
+                            }
+                        }
+                        $gru_fam = grupo_familiar::where("nro_doc", $value)->first();
+                        if ($gru_fam) {
+                            if (empty($gru_fam->fecha_egreso_sind)) {
+                                $fail("Error al dar de alta, Ya existe un Familiar con este dni, primero de de baja el familiar y luego de alta el titular.");
+                            }
+                        }
+                    }
+                }
+            ],
+
+            'cuil' => [
+                function ($attribute, $value, $fail) use ($rq) {
+                    if (!($rq->input($attribute) == null)) {
+                        if (!$rq->id) {
+                            $afiliado = afiliado::where("cuil", $rq->cuil)->first();
+                            if (!empty($afiliado)) {
+                                $fail("Otro afiliado titular ya esta dado de alta con el mismo nro de CUIL");
+                            }
+                        }
+                        $gru_fam = grupo_familiar::where("cuil", $value)->first();
+                        if ($gru_fam) {
+                            if (empty($gru_fam->fecha_egreso_sind)) {
+                                $fail("Error al dar de alta, Ya existe un Familiar con este CUIL, primero de de baja el familiar y luego de alta el titular.");
+                            }
+                        }
+                    }
+                }
+            ],
+
+            'cuil' => [
+                function ($attribute, $value, $fail) use ($rq) {
+                    if (!($rq->input($attribute) == null)) {
+                        if (!$rq->id) {
+                            $afiliado = afiliado::where("nro_afil_sindical", $rq->nro_afil_sindical)->first();
+                            if (!empty($afiliado)) {
+                                $fail("Otro afiliado titular ya esta dado de alta con el mismo nro de Afiliado Sindical");
+                            }
+                        }
+                    }
+                }
+            ],
         ];
     }
 }
--- app/Http/Requests/familiaresRequest.php
+++ app/Http/Requests/familiaresRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+use App\models\grupo_familiar;
+use App\models\afiliado_empresa;
 
 class familiaresRequest extends FormRequest
 {
@@ -21,12 +24,32 @@ class familiaresRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(Request $rq)
     {
         return [ 
             'fecha_nac' => ['bail', 'nullable', 'date', 'before:today'],
             'apellido_nombres' => ['required', 'max:150'],
-            'nro_doc' => ['required', 'numeric', 'digits_between:6,12']
+            'nro_doc' => ['required', 'numeric', 'digits_between:6,12'],
+            function ($attribute, $value, $fail) use ($rq) {
+                if (!($rq->input($attribute) == null)) {
+                    if (!$rq->id) {
+                        $grupo_familiar = grupo_familiar::where("nro_doc", $rq->nro_doc)->first();
+                        if (!empty($grupo_familiar)) {
+                            $fail("Otro familiar ya esta dado de alta con el mismo nro de DNI");
+                        }
+                    }
+                    $afiliado_empresa = afiliado_empresa::query()
+                    ->join('afiliado', 'id', 'afiliado_id')
+                    ->where('afiliado_id', $rq->afiliado_id)
+                    ->orderByDesc('fecha_ingreso')
+                    ->first();
+                    if ($afiliado_empresa) {
+                        if (empty($afiliado_empresa->fecha_egreso)) {
+                            $fail("Error al dar de alta, Ya existe un Afiliado Titular con este dni, el Titular debe estar dado de baja para luego de alta el familiar.");
+                        }
+                    }
+                }
+            }
         ];
     }
 }
